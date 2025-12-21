910a9bafe183dfe3957ddd776d54531078249a82
Vũ Ngọc Phiêu <heoconbeo987@gmail.com>
2025-12-21 21:24:30 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const contactBtn = document.getElementById("contactBtn");
 
-contactBtn.addEventListener("click", () => {
-  alert(
-    "📩 Liên hệ:\nFacebook: fb.com/ngocphieu2106\nEmail: vuphieu32@gmail.com"
-  );
+const text = "Vũ Ngọc Phiêu";
+let i = 0;
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+  if (i < text.length) {
+    typing.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 120);
+  }
+}
+typeEffect();
+
+
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+let isPlaying = false;
+
+musicBtn.addEventListener("click", () => {
+  if (!isPlaying) {
+    music.play();
+    musicBtn.innerHTML = '<i class="fas fa-pause"></i>';
+  } else {
+    music.pause();
+    musicBtn.innerHTML = '<i class="fas fa-music"></i>';
+  }
+  isPlaying = !isPlaying;
 });
