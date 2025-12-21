03f5475d5ba6bcc88bb7971ab88c2ded58d98c3f
Vũ Ngọc Phiêu <66714569+vungocphieu@users.noreply.github.com>
2025-12-21 21:16:20 +0700

    Add contact button click alert with contact info

---
+++ script.js
@@ -0,0 +1,7 @@
+const contactBtn = document.getElementById("contactBtn");
+
+contactBtn.addEventListener("click", () => {
+  alert(
+    "📩 Liên hệ:\nFacebook: fb.com/ngocphieu2106\nEmail: vuphieu32@gmail.com"
+  );
+});
